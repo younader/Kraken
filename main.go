c064b1c3b71f03b87ad1bcbd691ae55463e452dc
Yusuf <joenader510@gmail.com>
2019-04-19 17:15:56 +0200

    added hashing function and permutations

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
 import "fmt"
+
+
+import "crypto/sha1" 
+import "encoding/hex"
+
+
 func Perm(a []rune, f func([]rune)) {
     perm(a, f, 0)
 }
@@ -18,8 +24,16 @@ func perm(a []rune, f func([]rune), i int) {
         a[i], a[j] = a[j], a[i]
     }
 }
-
+func createHash(key string) string {
+	hasher := sha1.New()
+	hasher.Write([]byte(key))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
 func main() {
     Perm([]rune("abcd"), func(a []rune) {
-        fmt.Println(string(a))
-    })}
+        fmt.Println(createHash(string(a)))
+    })
+    fmt.Println("Hello fatouma")
+    fmt.Println(createHash("hello fatouma"))
+    fmt.Println(createHash("hello fatouma"))
+}
