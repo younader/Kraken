7e479009f53fbab2668528ca5f3a4c566d5cf9e6
Yusuf <joenader510@gmail.com>
2019-04-19 20:59:58 +0200

    added combinations with repetitions and hash comparison, now can brute force passwords statically

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "fmt"
 
 import "crypto/sha1" 
 import "encoding/hex"
-
+import "strings"
 
 func Perm(a []rune, f func([]rune)) {
     perm(a, f, 0)
@@ -29,11 +29,45 @@ func createHash(key string) string {
 	hasher.Write([]byte(key))
 	return hex.EncodeToString(hasher.Sum(nil))
 }
+func combrep(n int, lst []string) [][]string {
+    if n == 0 {
+        return [][]string{nil}
+    }
+    if len(lst) == 0 {
+        return nil
+    }
+    r := combrep(n, lst[1:])
+    for _, x := range combrep(n-1, lst) {
+        r = append(r, append(x, lst[0]))
+    }
+    return r
+}
 func main() {
-    Perm([]rune("abcd"), func(a []rune) {
-        fmt.Println(createHash(string(a)))
-    })
-    fmt.Println("Hello fatouma")
-    fmt.Println(createHash("hello fatouma"))
-    fmt.Println(createHash("hello fatouma"))
+    myhash:=createHash("fatma")
+    // Perm([]rune("abcd"), func(a []rune) {
+    //     fmt.Println(createHash(string(a)))
+    // })
+    // fmt.Println("Hello fatouma")
+    // fmt.Println(createHash("hello fatouma"))
+    // fmt.Println(createHash("hello fatouma"))
+    possible_combinations:=combrep(5,
+        strings.Split("a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9 $ @ &", " "))
+      
+        for index,element:=range possible_combinations{
+        //fmt.Println(strings.Join(element,""))
+       found:=0 
+       
+        Perm([]rune(strings.Join(element,"")), func(a []rune) {
+            if myhash==createHash(string(a)){
+                fmt.Println("FOUND PASSWORD:")
+                 fmt.Println(string(a))
+                 
+                 
+            }
+        
+            // fmt.Println(string(a))
+        })
+
+        if found==1{fmt.Println(index)}
+    }    
 }
